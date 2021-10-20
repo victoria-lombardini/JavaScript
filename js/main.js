7c8ad7baf682284be9c277bcde5b772633568604
Maria Victoria Lombardini <victorialombardini1@gmailcom>
2021-10-20 20:28:49 -0300

    Desafio 1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Pedir nombre en prompt y mostrar texto de saludo
+console.log("Hola");
+
+// Operaciones matematicas
+prompt("Ingrese su nombre")
+let nombre = "Victoria";
+console.log("Mi nombre es " + nombre);
+
+var numero1, numero2, resultado;
+numero1 = prompt ("Ingrese un numero");
+numero2 = 5;
+
+console.log("numero 2 es 5")
+resultado = (parseInt(numero1)+ parseInt(numero2));
+console.log("el resultado es:" + resultado);
+
+// Texto mediante prompt, concatenarlo y mostrar alert
+var texto1 = "Vivo en ";
+texto2 = prompt("¿Donde vive?");
+
+alert(texto1 + texto2);
